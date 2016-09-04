77c3c9b524e60fe17dcdbd663ef75bb4a72e7f17
shkiran4 <shkiran4@in.ibm.com>
2016-09-04 16:40:37 +0530

    Initial Commit

---
+++ cloumqtt_auth_publish_subscribe_on_off.ino
@@ -0,0 +1,61 @@
+# Here I have coded to receive on and off messages from the client/app
+# Of interest if the callback.
+# The payload is either populated with on or off messages.
+# This gets printed on the Serial Monitor.
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+
+char server[] = "m12.cloudmqtt.com";
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // handle message arrived
+  char command[10]; // to store on and off.
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+    command[i] = (char)payload[i];
+  }
+  command[length]  ='\0';
+  Serial.println();
+
+  if ( strncmp(command, "on", 2) == 0 ) {
+     Serial.println ("Your wish was to ON");
+  }else if ( strncmp ( command, "off", 3) == 0 ) {
+     Serial.println ("Your wish was to OFF");
+  }
+  //Serial.println (command );
+  
+}
+
+EthernetClient ethClient;
+PubSubClient client(server, 19757, callback, ethClient);
+
+void setup()
+{
+  Serial.begin(9600);
+  Ethernet.begin(mac);
+  
+  if (client.connect("arduinoClient", "oxefqvkn", "aKpQPSFiTpXp")) {
+    Serial.println ("Connected successfully");
+    Serial.println ("If you want to see any message published from apps, start the apps on remote side");
+    client.publish("outTopic","hello worldGowriDay");
+    client.subscribe("/frommothership");
+  }
+}
+
+void loop()
+{
+    // Uncomment only if you want to publish over and over 
+    //client.publish("outTopic","hello worldGowriDay");
+    //Serial.println ("Published to Outtopic");
+    // delay ( 1000);
+  // stay in loop listening to any commands being published from applications
+  // or from the laptop.
+  client.loop();
+}
